eed728279529d4191e3a67fca2e4bac9399e440d
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-08-16 16:23:07 +0200

    add  usecase1, refactor usecase0

--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,30 @@
     <div class="flex flex-col overflow-auto w-screen h-screen p-2">
       <!-- usecases -->
       <div class="m-2 rounded">
-        <!-- category 1 -->
+        <!-- category 0 -->
         <h3 class="text-2xl p-2 m-2">
           Application of EUCP innovations (WP4)
         </h3>
         <div class="text-left w-1/3 shadow-xl bg-white m-2">
           <UseCaseCard
-            :key="usecaseItems.example_usecase_id"
-            :title="usecaseItems.example_usecase_name"
-            :description="usecaseItems.example_usecase_description"
+            :key="item0.usecase_id"
+            :title="item0.usecase_name"
+            :description="item0.usecase_description"
             :usecase="usecase0"
           />
         </div>
-        <!-- category 2 -->
+        <!-- category 1 -->
+        <h3 class="text-2xl p-2 m-2">
+          Extracting information from large ensembles (WP2)
+        </h3>
+        <div class="text-left w-1/3 shadow-xl bg-white m-2">
+          <UseCaseCard
+            :key="item1.usecase_id"
+            :title="item1.usecase_name"
+            :description="item1.usecase_description"
+            :usecase="usecase1"
+          />
+        </div>
       </div>
     </div>
   </div>
@@ -31,13 +42,19 @@ export default {
   data () {
     return {
       usecaseItems: {},
+      item0: {},
       usecase0: [],
+      item1: {},
+      usecase1: [],
       error: false
     }
   },
   async mounted () {
-    this.usecaseItems = await this.$content('usecaseItems').fetch()
+    this.usecaseItems = await this.$content('index').fetch()
+    this.item0 = this.usecaseItems.usecase_0
     this.usecase0 = await this.$content('usecase_0').sortBy('slug', 'asc').fetch()
+    this.item1 = this.usecaseItems.usecase_1
+    this.usecase1 = await this.$content('usecase_1').sortBy('slug', 'asc').fetch()
   }
 }
 </script>
