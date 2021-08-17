44db04403f9eea8e860b0477795cf5e7a8410bd6
SarahAlidoost <f.alidoost@esciencecenter.nl>
2021-08-17 16:53:23 +0200

    remove commented lines

--- pages/index.vue
+++ pages/index.vue
@@ -8,31 +8,6 @@
     <div class="flex flex-col overflow-auto w-screen h-screen p-2">
       <!-- usecases -->
       <div class="m-2 rounded">
-        <!-- category 0 -->
-        <!-- <h3 class="text-2xl p-2 m-2">
-          {{ item0.category }}
-        </h3>
-        <div class="text-left w-1/3 shadow-xl bg-white m-2">
-          <UseCaseCard
-            :key="item0.usecase_id"
-            :title="item0.usecase_name"
-            :description="item0.usecase_description"
-            :usecase="usecase0"
-          />
-        </div> -->
-        <!-- category 1 -->
-        <!-- <h3 class="text-2xl p-2 m-2">
-          {{ item1.category }}
-        </h3>
-        <div class="text-left w-1/3 shadow-xl bg-white m-2">
-          <UseCaseCard
-            :key="item1.usecase_id"
-            :title="item1.usecase_name"
-            :description="item1.usecase_description"
-            :usecase="usecase1"
-          />
-        </div> -->
-        <!-- category 2 -->
         <div v-for="usecase in usecaseItems.usecases" :key="usecase">
           <h3 class="text-2xl p-2 m-2">
             {{ usecase.category }}
