0cfd5a5560e1cede42dd3acddcf7a413323362d6
Peter Kalverla <peter.kalverla@gmx.com>
2021-08-18 15:21:02 +0200

    Add loop to fetch content for all usecases

--- pages/index.vue
+++ pages/index.vue
@@ -8,7 +8,7 @@
     <div class="flex flex-col overflow-auto w-screen h-screen p-2">
       <!-- usecases -->
       <div class="m-2 rounded">
-        <div v-for="(item, index) in usecaseItems.usecases" :key="index">
+        <div v-for="(item, idx) in index.usecases" :key="idx">
           <h3 class="text-2xl p-2 m-2">
             {{ item.category }}
           </h3>
@@ -17,7 +17,7 @@
               :key="item.id"
               :title="item.name"
               :description="item.description"
-              :usecase="usecase_data"
+              :usecase="usecaseData[idx]"
             />
           </div>
         </div>
@@ -30,14 +30,20 @@
 export default {
   data () {
     return {
-      usecaseItems: {},
-      usecase_data: [],
+      index: {},
+      usecaseData: {},
       error: false
     }
   },
   async mounted () {
-    this.usecaseItems = await this.$content('index').fetch()
-    this.usecase_data = await this.$content('usecase_1').sortBy('slug', 'asc').fetch()
+    const index = await this.$content('index').fetch()
+    const usecases = index.usecases
+
+    for (const usecase in usecases) {
+      this.usecaseData[usecase] = await this.$content(usecase).sortBy('slug', 'asc').fetch()
+    }
+
+    this.index = index // why does it not work if we assing the result of fetch directly to this.index?
   }
 }
 </script>
