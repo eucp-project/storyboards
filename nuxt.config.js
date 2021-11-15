324e4fcdf4e2f4e0cc3e7ecee9b51f8bd404d10c
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-15 14:47:09 +0100

    Change page title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'usecases',
+    title: 'EUCP Storyboards',
     htmlAttrs: {
       lang: 'en'
     },
