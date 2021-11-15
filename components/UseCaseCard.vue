ee329ab5d5b311118ef7608ce4c8d65cfa7f4cb2
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-15 18:30:39 +0100

    Feature only one image on usecase card

--- components/UseCaseCard.vue
+++ components/UseCaseCard.vue
@@ -1,6 +1,6 @@
 <template class="phantom">
   <NuxtLink v-if="usecase.length > 0" :to="usecase[0].path">
-    <div class="flex flex-row p-2 shadow-xl max-w-xl">
+    <div class="flex flex-row p-2 shadow-xl max-w-xl min-h-full">
       <!-- Usecase cards -->
       <div class="prose">
         <h4 class="m-2">
@@ -10,11 +10,7 @@
           {{ description }}
         </p>
       </div>
-      <div>
-        <div class="imagepile relative m-4">
-          <img v-for="card of reversed(usecase)" :key="card.id" :src="card.image" alt="storyboard preview" class="image">
-        </div>
-      </div>
+      <img :src="usecase[0].image" alt="storyboard preview" class="max-w-sm">
     </div>
   </NuxtLink>
 </template>
@@ -46,58 +42,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-.imagepile {
-  width: 200px;
-  height: 200px;
-}
-.image {
-  position: absolute;
-  height: 180px;
-  width: 200px;
-  top: 0;
-  left: 0;
-  border: 5px solid #fff;
-  -moz-box-shadow: 0 2px 5px rgba(30, 30, 30, 0.25);
-  -webkit-box-shadow: 0 2px 5px rgba(30, 30, 30, 0.25);
-  box-shadow: 0 2px 5px rgba(30, 30, 30, 0.25);
-  z-index: 2;
-}
-
-.image:first-child {
-  position: relative;
-  -moz-transform: rotate(8deg);
-  -ms-transform: rotate(8deg);
-  -webkit-transform: rotate(8deg);
-  transform: rotate(8deg);
-}
-
-.image:nth-child(2) {
-  -moz-transform: rotate(2deg);
-  -ms-transform: rotate(2deg);
-  -webkit-transform: rotate(2deg);
-  transform: rotate(2deg);
-}
-
-.image:nth-child(3) {
-  -moz-transform: rotate(-3deg);
-  -ms-transform: rotate(-3deg);
-  -webkit-transform: rotate(-3deg);
-  transform: rotate(-3deg);
-}
-
-.image:nth-child(4) {
-  -moz-transform: rotate(-10deg);
-  -ms-transform: rotate(-10deg);
-  -webkit-transform: rotate(-10deg);
-  transform: rotate(-10deg);
-}
-
-.image:last-child {
-  -moz-transform: rotate(-5deg);
-  -ms-transform: rotate(-5deg);
-  -webkit-transform: rotate(-5deg);
-  transform: rotate(-5deg);
-}
-</style>
