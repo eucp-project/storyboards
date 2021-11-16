0c477a7b06bf526a99a980a70ac472a220a576f7
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-16 19:12:16 +0100

    Order by TRL

--- pages/index.vue
+++ pages/index.vue
@@ -10,13 +10,13 @@
 
     <div class="flex flex-col m-2 gap-2">
       <!-- categories -->
-      <div v-for="(category,key) in categories" :key="key" class="flex flex-wrap gap-4 mb-8">
+      <div v-for="(trl, key) in categories" :key="key" class="flex flex-wrap gap-4 mb-8">
         <h3 class="prose-xl w-full">
-          {{ category }}
+          {{ parse_trl(trl) }}
         </h3>
         <!-- usecases -->
         <UseCaseCard
-          v-for="(item, idx) in categorize(index.usecases, category)"
+          v-for="(item, idx) in categorize(index.usecases, trl)"
           :key="item.id"
           :title="item.name"
           :description="item.description"
@@ -42,22 +42,33 @@ export default {
     const usecases = index.usecases
     for (const usecase in usecases) {
       this.usecaseData[usecase] = await this.$content(usecase).sortBy('slug', 'asc').fetch().catch(e => console.log(e))
-      const category = usecases[usecase].category
-      if (!this.categories.includes(category)) {
-        this.categories.push(category)
+      const trl = usecases[usecase].trl
+      if (!this.categories.includes(trl)) {
+        this.categories.push(trl)
       }
     }
     this.index = index // why does it not work if we assign the result of fetch directly to this.index?
   },
   methods: {
-    categorize (usecases, category) {
+    categorize (usecases, trl) {
       const items = {}
       for (const usecase in usecases) {
-        if (usecases[usecase].category === category) {
+        if (usecases[usecase].trl === trl) {
           items[usecase] = usecases[usecase]
         }
       }
       return items
+    },
+    parse_trl (trl) {
+      if (trl === 'low') {
+        return 'Technical readiness level: low; research still in exploratory phase'
+      } else if (trl === 'medium') {
+        return 'Technical readiness level: medium; advanced science / generalization beyond initial proof of concept'
+      } else if (trl === 'high') {
+        return 'Technical readiness level: high; applied science / (almost) ready for operational use'
+      } else {
+        return 'Miscellaneous'
+      }
     }
   }
 }
