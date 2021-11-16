7f2c500339af02476df6073acbb34620c5f2996d
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-16 12:16:17 +0100

    Remove vueper dependency

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,7 +32,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vueper-slides.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
