651a64683abecf16bec142a798e44632fdae516e
Peter9192 <Peter9192@users.noreply.github.com>
2021-11-16 11:32:35 +0000

    deploy: aa0d8154f7bda0c935a8dde5f79b5b9ecd0cbf0b

--- _nuxt/20c5120.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{187:function(e,n,t){t(188),e.exports=t(189)},215:function(e,n,t){"use strict";t.r(n);t(21),t(52);n.default=function(e){var path=e.route.hash.replace("#!","");path.length&&e.redirect(path)}}},[[187,6,1,7]]]);
