49f6d761f812fbd6ee7865f5145d27cfb8169019
Peter Kalverla <peter.kalverla@gmx.com>
2021-11-16 12:17:20 +0100

    Polish (#34)
    * Update header
    * Change page title
    * Use tailwind typography (requires new npm install)
    * Consistent index and storyboard pages
    * Replace caroussel with 'pile of stacked images'
    * Feature only one image on usecase card
    * Organize usecasecard vertically
    * Revert "Organize usecasecard vertically"
    * Further cleaning of index page
    * Remove vueper dependency

--- components/UseCaseCard.vue
+++ components/UseCaseCard.vue
@@ -1,38 +1,18 @@
-<template>
-  <div class="flex flex-row overflow-auto p-4 shadow-xl">
-    <!-- Usecase cards -->
-    <div class="no-wrap text-left w-1/2">
-      <NuxtLink v-if="usecase.length > 0" :to="usecase[0].path">
-        <h4 class="text-2xl m-2">
+<template class="phantom">
+  <NuxtLink v-if="usecase.length > 0" :to="usecase[0].path">
+    <div class="flex flex-row bg-white shadow-xl max-w-xl rounded-lg h-48">
+      <!-- Usecase cards -->
+      <div class="prose m-4">
+        <h4>
           {{ title }}
         </h4>
-        <p class="text-left text-xl m-2">
+        <p>
           {{ description }}
         </p>
-      </NuxtLink>
+      </div>
+      <img :src="usecase[0].image" alt="storyboard preview" class="max-w-xs rounded-r-lg">
     </div>
-    <div class="container mx-auto w-1/2 m-3">
-      <vueper-slides
-        fractions
-        progress
-        fade
-        :slide-ratio="1 / 1"
-        slide-image-inside
-        :arrows="false"
-      >
-        <vueper-slide
-          v-for="card of usecase"
-          :key="card.id"
-        >
-          <template #content>
-            <NuxtLink :to="card.path">
-              <img :src="card.image" class="object-contain w-auto h-full max-w-full max-h-full mx-auto">
-            </NuxtLink>
-          </template>
-        </vueper-slide>
-      </vueper-slides>
-    </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script>
@@ -53,6 +33,11 @@ export default {
       type: Array,
       default: () => [],
       required: false
+    }
+  },
+  methods: {
+    reversed (arr) {
+      return arr.reverse()
     }
   }
 }
--- plugins/vueper-slides.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import { VueperSlides, VueperSlide } from 'vueperslides'
-import 'vueperslides/dist/vueperslides.css'
-Vue.component('VueperSlides', VueperSlides)
-Vue.component('VueperSlide', VueperSlide)
