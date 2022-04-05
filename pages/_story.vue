8cbde12316237ec3ba1351aca953933206d912e2
Peter Kalverla <peter.kalverla@gmx.com>
2022-04-05 13:59:21 +0200

    Enlarge images on click (#76)
    * Enlarge image on click; close with a second click or escape key
    * Update images for shoreline retreat story
    
    Co-authored-by: Yang <y.liu@esciencecenter.nl>

--- pages/_story.vue
+++ pages/_story.vue
@@ -20,17 +20,22 @@
     </div>
 
     <!-- Chapter image and description -->
-    <div v-for="(chapter, idx) in chapters" v-show="idx===currentChapter" :key="idx" class="flex gap-2 overflow-auto h-full">
-      <div class="w-2/3 bg-white rounded">
-        <img v-if="!chapter.props.image.endsWith('html')" :src="getContent(chapter.props.image)" class="object-contain w-auto h-full max-w-full max-h-full mx-auto">
-        <iframe v-else :src="getContent(chapter.props.image)" frameborder="0" class="w-full h-full" />
-      </div>
+    <div v-for="(chapter, idx) in chapters" v-show="idx===currentChapter" :key="idx" class="flex flex-row-reverse justify-end gap-2 overflow-auto h-full">
       <div class="p-4 w-1/3 bg-white rounded overflow-auto">
         <nuxt-content :document="chapter" class="prose mb-6" />
         <EditOnGitHub :target="gitHubURL()" />
         <p class="prose italic">
           For more information on editing stories, see <a href="https://blog.esciencecenter.nl/storyboards-for-science-communication-85e399e5c1b5" target="_blank">this blog post</a>.
         </p>
+      </div>
+      <div class="w-2/3 bg-white rounded">
+        <img v-if="!chapter.props.image.endsWith('html')" :src="getContent(chapter.props.image)" alt="story image" class="object-contain w-auto h-full max-w-full max-h-full mx-auto" @click="openBigImage">
+        <iframe v-else :src="getContent(chapter.props.image)" frameborder="0" class="w-full h-full" />
+        <div v-show="showBigImage" v-if="!chapter.props.image.endsWith('html')" class="fixed inset-0 flex bg-gray-900 bg-opacity-80" @click="closeBigImage">
+          <div class="fixed bg-white shadow-2xl inset-5 z-40 flex justify-center" @click="closeBigImage">
+            <img :src="getContent(chapter.props.image)" alt="story image" class="w-auto h-full object-contain">
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -51,8 +56,16 @@ export default {
   data () {
     return {
       error: false,
-      currentChapter: 0
+      currentChapter: 0,
+      showBigImage: false
     }
+  },
+  mounted () {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape') {
+        this.closeBigImage()
+      }
+    })
   },
   methods: {
     getContent (path) {
@@ -63,6 +76,12 @@ export default {
     },
     gitHubURL () {
       return `https://github.com/eucp-project/storyboards/blob/main/static/stories/${this.story.slug}.md`
+    },
+    openBigImage () {
+      this.showBigImage = true
+    },
+    closeBigImage () {
+      this.showBigImage = false
     }
   }
 }
