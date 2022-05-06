48aadacaca711262e461e6515b341fd773489601
Peter Kalverla <peter.kalverla@gmx.com>
2022-05-06 12:25:44 +0200

    Second attempt at EUCP favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
 
